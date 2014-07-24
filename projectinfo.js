492687ec99e6689e5e3462d9f2316b8aff9336f6
nathanrubix <karthik@rubixsoftsolutions.com>
2014-07-24 14:54:38 -0700

    Monaca Project info

---
+++ projectinfo.js
@@ -0,0 +1,14 @@
+{
+    "title-ja": "LienLizard Auction Lister",
+    "title-en": "LienLizard Auction Lister",
+    "description-ja": "",
+    "description-en": "LienLizard Auction Lister - Copyright Lienlizard.com",
+    "category": "template",
+    "platform": [
+        "android",
+        "ios",
+        "winrt"
+    ],
+    "name": "com.lienlizard.app",
+    "version": 1
+}
